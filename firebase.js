0326983caad9516acf42f63dade3987d38f5c19d
Mohamed Igamane <mohamedigamane1203@gmail.com>
2024-06-28 20:26:46 +0800

    S

--- firebase.js
+++ firebase.js
@@ -1,9 +1,17 @@
 const admin = require('firebase-admin');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
 const serviceAccount = require('./serviceAccountKey.json'); // Replace with the path to your service account key JSON file
+
+// Construct the database URL dynamically
+const databaseURL = `https://${process.env.FIREBASE_PROJECT_NAME}.firebaseio.com`;
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://my-node-app.firebaseio.com' // Replace with your database URL
+    databaseURL: databaseURL
 });
 
 const db = admin.firestore();
